e6e630605fccc6445a602e4478c2faf74b6dc4c5
huang huiliang <spiritsaway@outlook.com>
2016-03-03 16:42:43 +0800

    添加了正常的log，接下来需要手动加入关注列表

--- request_test/request_session.py
+++ request_test/request_session.py
@@ -23,6 +23,8 @@ class request_session:
 		self.content=None;
 		self.cur_link=None;
 		self.response=None;
+		self.weibo_total_num=0;
+		self.weibo_lately_num=0;
 		self.get_real_homepage=None;
 		self.ss=requests.Session();
 		self.smtp163=smtplib.SMTP();
@@ -42,9 +44,8 @@ class request_session:
 		self.email_from_passwd=log_config["from_passwd"];
 		self.email_smtp_addr=log_config["smtp_addr"];
 		self.email_smtp_port=log_config["smtp_port"];
-	def send_email_log(self):
-		self.smtp163.connect(self.email_smtp_addr,self.email_smtp_port);
-		self.smtp163.login(self.email_from_addr,self.email_from_passwd);
+
+	def send_email_except(self):
 		msg=MIMEMultipart();
 		msg["To"]=self.email_to_addr;
 		msg["Subject"]="exception from "+self.user.name+datetime.now().strftime("%Y-%m-%d %H:%M:%S");
@@ -55,6 +56,34 @@ class request_session:
 		exception_msg=MIMEText(traceback.format_exc());
 		msg.attach(attachment);
 		msg.attach(exception_msg);
+		try:
+			self.smtp163.connect(self.email_smtp_addr,self.email_smtp_port);
+			self.smtp163.login(self.email_from_addr,self.email_from_passwd);
+			self.smtp163.sendmail(self.email_from_addr,self.email_to_addr,msg.as_string());
+			self.smtp163.close();
+		except:
+			dumpfile=open(msg["Subject"],"w");
+			dumpfile.write(msg.as_string());
+			dumpfile.close();
+	def send_email_log(self):
+		msg=MIMEMultipart();
+		msg["To"]=self.email_to_addr;
+		msg["Subject"]="log from "+self.user.name+datetime.now().strftime("%Y-%m-%d %H:%M:%S");
+		msg["From"]=self.email_from_addr;
+		log_msg_str="total weibo num is "+str(self.weibo_total_num)+"weibo lately num is "+str(self.weibo_lately_num);
+		self.weibo_lately_num=0;
+		log_msg=MIMEText(log_msg_str);
+		msg.attach(log_msg);
+		try:
+			self.smtp163.connect(self.email_smtp_addr,self.email_smtp_port);
+			self.smtp163.login(self.email_from_addr,self.email_from_passwd);
+			self.smtp163.sendmail(self.email_from_addr,self.email_to_addr,msg.as_string());
+			self.smtp163.close();
+		except:
+			dumpfile=open(msg["Subject"],"w");
+			dumpfile.write(msg.as_string());
+			dumpfile.close();
+
 	def set_user(self,in_user):
 		self.user=in_user;
 
